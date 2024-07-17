cc0b1b83854cf3b69d786c854c7e6ca23e6c6e82
Sean Leung <scleung04@gmail.com>
2024-07-17 15:46:35 -0400

    commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,27 +1,63 @@
-﻿using Xbim.Ifc;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xbim.Ifc;
 using Xbim.Ifc4.Interfaces;
 
 namespace ModelInspector
 {
-    //TODO: add feature to write down the report as a CSV file
-    //TODO: use this in a program that can inspect a series of Ifc models within a folder structure. Input should be the root folder, and output should be a series of CSV files named after the original ifc models.
     public class Program
     {
         static void Main(string[] args)
         {
-            string ifcModelPath = @"C:\Users\scleu\Downloads\2024 Summer Research\R2024\02-NHSJC-ARQ.ifc";
+            // Input root folder containing IFC models
+            string rootFolderPath = @"C:\Users\scleu\Downloads\2024 Summer Research\R2024";
 
-            using (var model = IfcStore.Open(ifcModelPath))
+            // Process each IFC file in the folder structure
+            ProcessIFCFilesInFolder(rootFolderPath);
+        }
+
+        static void ProcessIFCFilesInFolder(string folderPath)
+        {
+            // Get all IFC files in the folder and its subfolders
+            string[] ifcFiles = Directory.GetFiles(folderPath, "*.ifc", SearchOption.AllDirectories);
+
+            foreach (var filePath in ifcFiles)
+            {
+                ProcessIFCFile(filePath);
+            }
+        }
+
+        static void ProcessIFCFile(string ifcFilePath)
+        {
+            using (var model = IfcStore.Open(ifcFilePath))
             {
                 var proxies = model.Instances.OfType<IIfcBuildingElementProxy>();
                 if (proxies.Any())
                 {
-                    int count = 0;
-                    foreach (var proxyElement in proxies)
+                    // CSV file path
+                    string csvFilePath = Path.ChangeExtension(ifcFilePath, ".csv");
+
+                    // Write proxies to CSV file
+                    using (var writer = new StreamWriter(csvFilePath))
                     {
-                        count++;
-                        Console.WriteLine($"{count} {proxyElement.EntityLabel}\t{proxyElement.ExpressType}\t{proxyElement.Name}");
+                        writer.WriteLine("Count,EntityLabel,ExpressType,Name");
+
+                        int count = 0;
+                        foreach (var proxyElement in proxies)
+                        {
+                            count++;
+                            string line = $"{count},{proxyElement.EntityLabel},{proxyElement.ExpressType},{proxyElement.Name}";
+                            writer.WriteLine(line);
+                        }
                     }
+
+                    Console.WriteLine($"Processed {ifcFilePath} and generated report at {csvFilePath}");
+                }
+                else
+                {
+                    Console.WriteLine($"No IfcBuildingElementProxy instances found in {ifcFilePath}");
                 }
             }
         }
